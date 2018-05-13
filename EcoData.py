c5c009bb6a68e822b5e562649b3be1d0686cdb50
Herve Biancotto <h.biancotto@hotmail.fr>
2018-05-13 22:31:37 +0100

    start candlestick visualisation

--- EcoData.py
+++ EcoData.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 21 13:14:18 2017
-
+ 
 @author: herve.biancotto
-
+ 
 @aim: Implements functions to import, standardise and visualise economic data.
 """
-
+ 
 #import bloompy as bp
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from mpl_finance import candlestick_ohlc
 import seaborn as sns
 
 RECESSION_DATA = r'P:\Python Scripts\EcoProject/usrecspan.csv'
@@ -19,32 +21,33 @@ def PoP(data, **kwargs):
     Returns the period over period data series.
     :param data: the data to transform.
     :type data: pandas DataFrame of 1 column.
-    
+   
     :key word parameters:
-    :param per: period corresponding to the period over period transformation. 
+    :param per: period corresponding to the period over period transformation.
     :param freq: frequency of the series to return.
     :type per: string in list ['D', 'M', 'Q', 'A']
     :type freq: string in list ['D', 'M', 'Q', 'A']
     :param method: defines the method to perform the period over period transformation.
     :type method: ['ratio', 'diff']
     """
-    #Correspondance tables
+    
+    # Correspondance tables
     CORRES_B = {'D':1, 'M':21, 'Q':63, 'H':130, 'A':262} #business days
     CORRES_D = {'D':1, 'M':30, 'Q':91, 'H':182, 'A':365} #calendar days
     CORRES_M = {'M':1, 'Q':3, 'H':6, 'A':12}
     CORRES_Q = {'Q':1, 'H':2, 'A':4}
     CORRES_LABEL = {'D':'DoD', 'M':'MoM', 'Q':'QoQ', 'H':'HoH', 'A':'YoY'}
-    
+
     if "per" in kwargs:
         period = kwargs["per"].upper()
     else:
         period = 'A'
-        
+       
     if "freq" in kwargs:
         frequency = kwargs["freq"].upper()
     else:
         frequency = 'D'
-    
+
     #map correspondance tables
     if frequency == 'D':
         CORRES = CORRES_D
@@ -54,16 +57,16 @@ def PoP(data, **kwargs):
         CORRES = CORRES_M
     elif frequency == 'Q':
         CORRES = CORRES_Q
-    
+   
     meth = 'ratio'
     if "method" in kwargs:
         if kwargs["method"] == 'diff':
             meth = kwargs["method"]
-    
+   
     #Converts series to DataFrame if necessary
     if isinstance(data, pd.Series):
         data = data.to_frame(data.name)
-    
+   
     # Aligns frequency and shift the second series by the period
     data = data.asfreq(frequency, method='pad')
     data_per = data.shift(CORRES[period])
@@ -75,67 +78,75 @@ def PoP(data, **kwargs):
     # Returns the period over period transformation
     return result
 
-
+ 
 def scale_data(data, mult=1, lead_lag=0, cum_data=False, freq='D'):
     """
-    Scales data by applying a multiplier, lagging or leading and cumulating the 
+    Scales data by applying a multiplier, lagging or leading and cumulating the
     pandas time series.
-    
+   
     :param data: the data to transform.
     :type data: pandas DataFrame.
-    
+   
     Named arguments:
     :param mult: multiplier to apply to the series.
     :param mult: a scalar, type float or int.
-    
+   
     :param lead_lag: number of period to lead(positive) or lag(negative) data.
     :type lead_lag: signed int.
     """
-
+ 
         #Converts series to DataFrame if necessary
     if isinstance(data, pd.Series):
         data = data.to_frame(data.name)
-    
+   
     if cum_data:
         value = mult*data.tshift(lead_lag)
         value = value.cumsum()
     else:
         value = mult*data.tshift(lead_lag)
     return value
-
-        
-
+ 
+       
+ 
 def graph(*args, **kwargs):
     """
     Graphs pandas DataFrame that are passed through.
     Allows overlays of recession periods.
-    
+   
     :param data: input data as a pandas DataFrame.
     :type data: pandas DataFrame.
-    
+   
     :param recession: Overlays the chart with recession periods.
     :type recession: Boolean.
-    
+   
     :param multiple_series: Adds an extra Y axe for each series.
     :type multiple_series: Boolean.
-    
-    :param chart_size: Size of the ouput chart.
+   
+    :param chart_size: Size of the output chart.
     :type chart_size: tuple of (int, int).
-    
+   
     :param title: Gives a title to the chart.
     :type title: str.
+    
+    :param subgraph: Adds a subgraph to the chart.
+    :type subgraph: pandas Series.
+    
+    :param candle: Change graph type to candlesticks.
+    :type candle: Boolean.
+    
+    :param legend: Display legend.
+    :type legend: Boolean.
     """
     
-    
-    ## Treat data
+    ## Check data
     data = [pd.DataFrame(data_set) for data_set in args]
     try:
         assert data is not None
     except AssertionError:
         print("No data to graph")
         return
-    
-    ## kwargs treatment
+   
+    ## Deal with kwargs
     if 'colour' in kwargs:
         if len(kwargs['colour']) != len(args):
             raise Exception('The colour provided should be of same lenght\
@@ -147,15 +158,15 @@ def graph(*args, **kwargs):
             return
     else:
         colour = {1:'#4a4a4a',
-                  2:'#951826', 
-                  3:'#ea7600', 
-                  4:'#007398', 
+                  2:'#951826',
+                  3:'#ea7600',
+                  4:'#007398',
                   5:'#33cc78',
                   6:'#f4ee00',
-                  7:'#669999', 
+                  7:'#669999',
                   8:'#ff99ff',
                   9:'#b35900',}
-    
+   
     recess = False
     if 'recession' in kwargs:
         if kwargs['recession']:
@@ -163,7 +174,7 @@ def graph(*args, **kwargs):
             'recession'], converters={'date':lambda x: pd.to_datetime(x,\
             dayfirst=True)})
             #rec_data = rec_data.asfreq(data[0].index.freqstr)
-            recess = True    
+            recess = True   
     
     extra_ax = False
     sns.set_style('ticks', {'axes.grid': True})
@@ -172,29 +183,56 @@ def graph(*args, **kwargs):
             extra_ax = True
             sns.set_style({'axes.grid': False})
             side = 'right' # alternate right and left when adding a new Y axis.
-    
+   
     size = None
     if 'chart_size' in kwargs:
-            size = kwargs['chart_size']
-    
+        size = kwargs['chart_size']
+   
     g_title = ''
     if 'title' in kwargs:
-            g_title = kwargs['title']
-           
+        g_title = kwargs['title']
+
+    sub = None
+    n_sub = 0
+    if 'subgraph' in kwargs:
+        sub = kwargs['subgraph']
+        if isinstance(sub, pd.DataFrame) or isinstance(sub, pd.Series):
+            n_sub = 1
+        elif isinstance(sub, (list, tuple)):
+            n_sub += len(sub)
+      
+    candle = False
+    if 'candle' in kwargs:
+        candle = kwargs['candle']
         
+    legend = True
+    if 'legend' in kwargs:
+        legend = kwargs['legend']
+    
     ## Graph creation
     if size is not None:
-        fig, ax = plt.subplots(figsize=size)
+        fig = plt.figure(figsize=size)
     else:
-        fig, ax = plt.subplots()
+        fig = plt.figure()
+   
+    rows = 5
+    grid = plt.GridSpec(rows, 1)   
+    ax = fig.add_subplot(grid[:rows-n_sub, 0])
     
-    line = plt.plot(data[0], label=data[0].columns[0], color=colour[1]) #base line
+    #main line
+    if candle:
+        if data[0].shape[1]!=5:
+            raise IndexError('Need 5 data series. Got {}'.format(data[0].shape[1]))
+        line = candlestick_ohlc(0)
+    else:
+        line = plt.plot(data[0], label=data[0].columns[0], color=colour[1]) #base line
+        
     if extra_ax:
         ax.set_ylabel(data[0].columns[0], color=colour[1])
-    i = 2 # counter to handle coulours
+    i = 2 # counter to handle colours
     for series in data[1:]:
         if extra_ax: #case multiple Y axis
-            axn = ax.twinx()   
+            axn = ax.twinx()  
             axn.set_frame_on(True)#adds to right
             axn.patch.set_visible(False)
             axn.spines[side].set_position(('axes', 1+0.07*(i-2))) #pushes out the new Y axis
@@ -202,8 +240,10 @@ def graph(*args, **kwargs):
         line += plt.plot(series, colour[i], label=series.columns[0])
         i+=1 #change colour for next series
     labels = [l.get_label() for l in line] #gets label all together
-    plt.legend(line, labels, loc=0)
-    
+    if legend:
+        plt.legend(line, labels, loc=0)
+
+   
     # Displays recessions as shaded area
     if recess:
         data_rec = rec_data[rec_data['date'] >= data[0].index[0]]
@@ -229,13 +269,33 @@ def graph(*args, **kwargs):
                     sns.despine(ax=axn, left=False, right=False) # axis not displayed
             except IndexError:
                 print('no recession during that period')
-    
+
     #Title
-    ax.axes.set_title(g_title)
-        
-    
+    if g_title is not None:
+        ax.axes.set_title(g_title)
+
+    # Adds subplot
+    if sub is not None:
+        for i in range(n_sub):
+            subg = fig.add_subplot(grid[rows- n_sub + i,0], sharex=ax)
+            subg.plot(sub)
+            # Oscillator formating
+            up_lim = np.array([70]*len(sub.values))
+            dn_lim = np.array([30]*len(sub.values))
+            subg.set_ylim([0,100])
+            subg.axhline(30, color='green')
+            subg.axhline(70,  color='red')
+            subg.grid(False)
+            x = sub.index.values
+            y = sub.values.flatten()
+            subg.fill_between(x, up_lim, y, \
+                            where=(y>=up_lim), facecolor='red', interpolate=True, alpha='0.25')
+            subg.fill_between(x, y, dn_lim,\
+                            where=(y<=dn_lim), facecolor='green', interpolate=True, alpha='0.25')
+
 if __name__ == "__main__":
     pass
+
  
 #    spx = bp.getHistoricalData('SPX Index','px last','20010101', \
 #                                          periodicity='DAILY')
